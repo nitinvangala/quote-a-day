6a394863ae8572e4f84e883937dc4eac5f050417
Nitin.Vangala <32849753+vangalar@users.noreply.github.com>
2017-11-18 16:28:36 -0500

    Heroku changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var server = app.listen(PORT, function(req, res){
     console.log('server started')
